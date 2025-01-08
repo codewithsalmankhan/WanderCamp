928f8e78506bcc1f03d71695d4b815dfa887b58a
codewithsalmankhan <codewithsalmankhan@gmail.com>
2025-01-08 16:06:03 -0600

    added authorization to campground routes

--- middleware.js
+++ middleware.js
@@ -1,3 +1,8 @@
+const { campgroundSchema, reviewSchema } = require('./schemas.js')
+const ExpressError = require('./utils/ExpressError')
+const Campground = require('./models/campground');
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
@@ -7,9 +12,39 @@ module.exports.isLoggedIn = (req, res, next) => {
     next();
 };
 
-module.exports.storeReturnTo = (req,res,next) => {
-    if(req.session.returnTo) {
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
         res.locals.returnTo = req.session.returnTo;
     }
     next();
+}
+
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author.equals(req.user._id)) {
+        req.flash('error', 'You do not have permission to perform this action!')
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg, 400)
+    } else {
+        next();
+    }
 }
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,23 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
-const { campgroundSchema } = require('../schemas.js')
 const Campground = require('../models/campground');
 
-const { isLoggedIn } = require('../middleware')
-
+const { isLoggedIn, isAuthor, validateCampground } = require('../middleware')
 const catchAsync = require('../utils/catchAsync');
-const ExpressError = require('../utils/ExpressError');
-
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
 router.get('/', async (req, res) => {
     const campgrounds = await Campground.find({})
@@ -32,6 +18,7 @@ router.get('/new', isLoggedIn, async (req, res) => {
 //Handling the post request sent by form
 router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Campground is successfully created!');
     res.redirect(`/campgrounds/${campground._id}`);
@@ -39,7 +26,7 @@ router.post('/', isLoggedIn, validateCampground, catchAsync(async (req, res, nex
 
 //Reading id of each object and rendering it on details page
 router.get('/:id', isLoggedIn, catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews');
+    const campground = await Campground.findById(req.params.id).populate('reviews').populate('author');
     if (!campground) {
         req.flash('error', 'Cannot find the campground');
         return res.redirect('/campgrounds');
@@ -48,7 +35,7 @@ router.get('/:id', isLoggedIn, catchAsync(async (req, res) => {
 }));
 
 //Rendering a form for updating an object
-router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     if (!campground) {
         req.flash('error', 'Cannot find the campground');
@@ -58,7 +45,7 @@ router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
 }));
 
 //using method override to send a put request to save the udpdated object
-router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Campground is successfully updated!');
@@ -66,7 +53,7 @@ router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res) =
 }));
 
 //deleting the object
-router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, isAuthor, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     req.flash('success', 'Campground is successfully deleted!');
