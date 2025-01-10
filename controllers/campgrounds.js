53b159043d4938e619feecaa276fea5d693853f9
codewithsalmankhan <codewithsalmankhan@gmail.com>
2025-01-10 16:54:15 -0600

    added image upload to edit form

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -11,7 +11,7 @@ module.exports.renderNewForm = async (req, res) => {
 
 module.exports.createCampground = async (req, res, next) => {
     const campground = new Campground(req.body.campground);
-    campground.images = req.files.map(f => ({url: f.path, filename: f.filename}));
+    campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.author = req.user._id;
     await campground.save();
     req.flash('success', 'Campground is successfully created!');
@@ -46,6 +46,9 @@ module.exports.renderEditForm = async (req, res) => {
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    campground.images.push(...imgs);
+    await campground.save();
     req.flash('success', 'Campground is successfully updated!');
     res.redirect(`/campgrounds/${campground._id}`);
 };
